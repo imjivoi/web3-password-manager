f07bac97b7615cafb603c966014d80ab61d29d98
Sergei Lezhnev <jivoi001@gmail.com>
2023-01-10 18:22:20 -0300

    feat: router guard

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
-
+import { useWalletStore } from './store/wallet'
+import Storage from './services/storage'
 import './styles/app.css'
 import clickOutside from './directives/click-outside'
 
 const app = createApp(App)
-
-app
   .directive('click-outside', clickOutside)
   .use(createPinia())
   .use(router)
-  .mount('#app')
+
+const walletStore = useWalletStore()
+router.beforeEach((to) => {
+  if (to.path.includes('app') && !walletStore.isLogged) {
+    return '/'
+  }
+})
+
+app.mount('#app')
