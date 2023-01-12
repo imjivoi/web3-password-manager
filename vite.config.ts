e1e2009e698d2906088ef7d11c3caf2a0aa94d10
imjivoi <jivoi001@gmail.com>
2023-01-12 20:31:07 -0300

    chore: update pckgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import * as path from 'path'
 import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
   plugins: [vue(), vanillaExtractPlugin(), svgLoader()],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
-    },
-  },
+  // optimizeDeps: {
+  //   esbuildOptions: {
+  //     // Node.js global to browser globalThis
+  //     define: {
+  //       global: 'globalThis',
+  //     },
+  //     // Enable esbuild polyfill plugins
+  //     plugins: [
+  //       // NodeGlobalsPolyfillPlugin({
+  //       //   buffer: true,
+  //       // }),
+  //     ],
+  //   },
+  // },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
